dfcfe3fa824698b8d43fdff179baf73e3d8a78c3
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2017-11-03 16:40:59 +0000

    Added support for if there isn't a dispatch.handler attribute in the Dispatch middleware.

--- src/Middleware/Dispatch.php
+++ src/Middleware/Dispatch.php
@@ -46,7 +46,10 @@ class Dispatch
 	 */
 	public function __invoke(Request $request, $response, $next)
 	{
-		$handler = $request->getAttribute('dispatch.handler');
+		$handler = $request->getAttribute('dispatch.handler', false);
+		if ($handler === false) {
+			return $next($request, $response);
+		}
 
 		if (is_string($handler) && strpos($handler, '|') !== false) {
 			$secondaryHandler = substr($handler, strpos($handler, '|') + 1);
