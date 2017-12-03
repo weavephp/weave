7f12704638731f4e3949aeee5f7bf736744b9c12
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2017-12-03 11:15:16 +0000

    Added test coverage of a single-pass pipeline execute and the internal workings of the resolver closure.

--- tests/Middleware/MiddlewareTest.php
+++ tests/Middleware/MiddlewareTest.php
@@ -41,6 +41,61 @@ class MiddlewareTest extends TestCase
 	}
 
 	/**
+	 * Test basic consturction with the mock objects works.
+	 *
+	 * @return null
+	 */
+	public function testResolveClosure()
+	{
+		$request = $this->createMock(\Psr\Http\Message\ServerRequestInterface::class);
+
+		$middlewareAdaptor = $this->createMock(MiddlewareAdaptorInterface::class);
+		$middlewareAdaptor->method('setResolver')
+		->with(
+			$this->callback(
+				function($callable) use ($request) {
+					$dispatchable = $callable('foo');
+					$this->assertEquals('pong', $dispatchable($request));
+					return true;
+				}
+			)
+		);
+
+		$resolveAdaptor = $this->createMock(\Weave\Resolve\ResolveAdaptorInterface::class);
+		$resolveAdaptor->expects($this->once())
+		->method('resolve')
+		->with($this->equalTo('foo'))
+		->willReturn('bar');
+
+		$dispatchAdaptor = $this->createMock(\Weave\Dispatch\DispatchAdaptorInterface::class);
+		$dispatchAdaptor->expects($this->once())
+		->method('dispatch')
+		->with(
+			$this->equalTo('bar'),
+			$this->equalTo(null),
+			$this->equalTo(\Weave\Dispatch\DispatchAdaptorInterface::SOURCE_MIDDLEWARE_STACK),
+			$this->equalTo($request)
+		)
+		->willReturn('pong');
+
+		$requestFactory = $this->createMock(\Weave\Http\RequestFactoryInterface::class);
+		$responseFactory = $this->createMock(\Weave\Http\ResponseFactoryInterface::class);
+		$emitter = $this->createMock(\Weave\Http\ResponseEmitterInterface::class);
+
+		$middleware = new Middleware(
+			$middlewareAdaptor,
+			function () {
+				return 'pipelineFoo';
+			},
+			$resolveAdaptor,
+			$dispatchAdaptor,
+			$requestFactory,
+			$responseFactory,
+			$emitter
+		);
+	}
+
+	/**
 	 * Test chaining a new pipeline onto an existing pipeline.
 	 *
 	 * @return null
@@ -84,11 +139,11 @@ class MiddlewareTest extends TestCase
 	}
 
 	/**
-	 * Test setting up of an initial pipeline.
+	 * Test setting up of an initial double-pass pipeline.
 	 *
 	 * @return null
 	 */
-	public function testExecute()
+	public function testExecuteDoublePass()
 	{
 		$request = $this->createMock(\Psr\Http\Message\ServerRequestInterface::class);
 		$response = $this->createMock(\Psr\Http\Message\ResponseInterface::class);
@@ -132,6 +187,59 @@ class MiddlewareTest extends TestCase
 		$this->assertEquals('ping', $result);
 	}
 
+	/**
+	 * Test setting up of an initial single-pass pipeline.
+	 *
+	 * @return null
+	 */
+	public function testExecuteSinglePass()
+	{
+		$request = $this->createMock(\Psr\Http\Message\ServerRequestInterface::class);
+
+		$middlewareAdaptor = $this->createMock(MiddlewareAdaptorInterface::class);
+		$middlewareAdaptor->method('setResolver');
+		$middlewareAdaptor->method('isDoublePass')->willReturn(false);
+		$middlewareAdaptor->method('executePipeline')
+		->with(
+			$this->equalTo('pipelineFoo'),
+			$this->equalTo($request),
+			$this->equalTo(null)
+		)
+		->willReturn('ping');
+
+		$resolveAdaptor = $this->createMock(\Weave\Resolve\ResolveAdaptorInterface::class);
+		$dispatchAdaptor = $this->createMock(\Weave\Dispatch\DispatchAdaptorInterface::class);
+
+		$requestFactory = $this->createMock(\Weave\Http\RequestFactoryInterface::class);
+		$requestFactory->method('newIncomingRequest')->willReturn($request);
+
+		$responseFactory = $this->createMock(\Weave\Http\ResponseFactoryInterface::class);
+		$responseFactory->expects($this->never())->method('newResponse');
+
+		$emitter = $this->createMock(\Weave\Http\ResponseEmitterInterface::class);
+
+		$middleware = new Middleware(
+			$middlewareAdaptor,
+			function () {
+				return 'pipelineFoo';
+			},
+			$resolveAdaptor,
+			$dispatchAdaptor,
+			$requestFactory,
+			$responseFactory,
+			$emitter
+		);
+
+		$result = $middleware->execute();
+
+		$this->assertEquals('ping', $result);
+	}
+
+	/**
+	 * Test calling the emitter on emit.
+	 *
+	 * @return null
+	 */
 	public function testEmit()
 	{
 		$response = $this->createMock(\Psr\Http\Message\ResponseInterface::class);
